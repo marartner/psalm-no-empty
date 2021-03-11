04486ff70c2881e898d6671900207b9052bd92e2
Marcus Artner <git@martner.at>
2021-03-11 10:06:39 +0100

    Initial commit

---
+++ tests/PluginTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Weirdan\PsalmPluginSkeleton\Tests;
+
+use SimpleXMLElement;
+use Weirdan\PsalmPluginSkeleton\Plugin;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psalm\Plugin\RegistrationInterface;
+
+class PluginTest extends TestCase
+{
+    /**
+     * @var ObjectProphecy
+     */
+    private $registration;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->registration = $this->prophesize(RegistrationInterface::class);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function hasEntryPoint()
+    {
+        $this->expectNotToPerformAssertions();
+        $plugin = new Plugin();
+        $plugin($this->registration->reveal(), null);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function acceptsConfig()
+    {
+        $this->expectNotToPerformAssertions();
+        $plugin = new Plugin();
+        $plugin($this->registration->reveal(), new SimpleXMLElement('<myConfig></myConfig>'));
+    }
+}
